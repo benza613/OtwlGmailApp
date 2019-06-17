898a557174c5642cbb2f448626f3dc13ea7acc8c
Shubham Shailesh Parab <shupar1996@gmail.com>
2019-06-17 14:52:32 +0530

    s170620191452

--- src/app/_http/domain.service.ts
+++ src/app/_http/domain.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment as env } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,17 +20,17 @@ export class DomainService {
   };
 
   fetchRefTypes(): Observable<any> {
-    // return this.http.post(`${this.apiBaseUrl}/`, this.httpOptions)
-    // .pipe(map(r => r));
+    return this.http.post(`${this.apiBaseUrl}/`, this.httpOptions)
+    .pipe(map(r => r));
   }
 
   fetchRefTypeData(): Observable<any>  {
-    // return this.http.post(`${this.apiBaseUrl}/`, this.httpOptions)
-    // .pipe(map(r => r));
+    return this.http.post(`${this.apiBaseUrl}/`, this.httpOptions)
+    .pipe(map(r => r));
   }
 
   fetchPartyTypeData(): Observable<any> {
-    // return this.http.post(`${this.apiBaseUrl}/`, this.httpOptions)
-    // .pipe(map(r => r));
+    return this.http.post(`${this.apiBaseUrl}/`, this.httpOptions)
+    .pipe(map(r => r));
   }
 }

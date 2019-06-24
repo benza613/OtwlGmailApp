a244f3314389dc6f8fbdb763e25603bbbdb9919f
benza613 <benito.alvares@gmail.com>
2019-06-24 18:02:56 +0530

    msp

--- src/app/_pipe/map-view-filter.pipe.ts
+++ src/app/_pipe/map-view-filter.pipe.ts
@@ -14,22 +14,36 @@ export class mapViewFilter implements PipeTransform {
         let isNullOrEmptyC: boolean = (filter.c == undefined || filter.c == null || filter.c == '');
         let isNullOrEmptyA: boolean = (filter.a == undefined || filter.a == null || filter.a == '');
         let isNullOrEmptyB: boolean = (filter.b == undefined || filter.b == null || filter.b.length == 0);
-        console.log('isNullOrEmptyC', isNullOrEmptyC);
-        console.log('isNullOrEmptyA', isNullOrEmptyA);
+        // console.log('isNullOrEmptyC', isNullOrEmptyC);
+        // console.log('isNullOrEmptyB', isNullOrEmptyB);
+        // console.log('isNullOrEmptyA', isNullOrEmptyA);
 
         // filter items array, items which match and return true will be
         // kept, false will be filtered out
         //case 1: all are not empty
         //case 2: A & B is empty .. only C is filled
         //case 3: only A is empty
-        if (!isNullOrEmptyA && !isNullOrEmptyC) {
+
+        if (!isNullOrEmptyA && !isNullOrEmptyB && !isNullOrEmptyC) {
+            return items.filter(item => item.ThreadSubject.indexOf(filter.c) !== -1 && item.SelectedTypeIdList.filter(f => filter.b.includes(f)).length > 0 && item.ThreadReferenceText.indexOf(filter.a) !== -1);
+        }
+        else if (!isNullOrEmptyA && !isNullOrEmptyB) {
+            return items.filter(item => item.SelectedTypeIdList.filter(f => filter.b.includes(f)).length > 0 && item.ThreadReferenceText.indexOf(filter.a) !== -1);
+        }
+        else if (!isNullOrEmptyB && !isNullOrEmptyC) {
+            return items.filter(item => item.ThreadSubject.indexOf(filter.c) !== -1 && item.SelectedTypeIdList.filter(f => filter.b.includes(f)).length > 0);
+        }
+        else if (!isNullOrEmptyA && !isNullOrEmptyC) {
             return items.filter(item => item.ThreadSubject.indexOf(filter.c) !== -1 && item.ThreadReferenceText.indexOf(filter.a) !== -1);
         }
-        else if (isNullOrEmptyA && !isNullOrEmptyC) {
-            return items.filter(item => item.ThreadSubject.indexOf(filter.c) !== -1);
-        }
-        else if (isNullOrEmptyC && !isNullOrEmptyA) {
+        else if (!isNullOrEmptyA) {
             return items.filter(item => item.ThreadReferenceText.indexOf(filter.a) !== -1);
+        }
+        else if (!isNullOrEmptyB) {
+            return items.filter(item => item.SelectedTypeIdList.filter(f => filter.b.includes(f)).length > 0);
+        }
+        else if (!isNullOrEmptyC) {
+            return items.filter(item => item.ThreadSubject.indexOf(filter.c) !== -1);
         }
 
         //if all are empty=> true

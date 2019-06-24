cd965fed0ab01d13fd8a37a6d5461211132a5b0d
Shubham Shailesh Parab <shupar1996@gmail.com>
2019-06-24 18:26:43 +0530

    s240620191826

--- src/app/emails/editor/editor.component.ts
+++ src/app/emails/editor/editor.component.ts
@@ -356,8 +356,8 @@ export class EditorComponent implements OnInit {
     //https://github.com/valor-software/ng2-file-upload/blob/development/src/file-upload/file-uploader.class.ts
   }
 
-  addAddr(event) {
-    const idx = this.msgAddrList.findIndex(obj => obj.emailId === event.emailId)
+  addAddr(event, select) {
+    const idx = this.msgAddrList.findIndex(obj => obj.emailId === event.emailId);
     if (idx === -1) {
       this.msgAddrList = [...this.msgAddrList, event.emailId];
     }

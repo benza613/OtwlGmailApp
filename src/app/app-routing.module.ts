bcb5baf75e0c230ef68896336266b00759cb17cf
Shubham Shailesh Parab <shupar1996@gmail.com>
2019-07-05 16:16:07 +0530

    s050720191616

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     path: 'mapped',
     component: EmailMappedComponent,
     pathMatch: 'full',
-    canActivate: [ActivateGuardService]
+    // canActivate: [ActivateGuardService]
 
   },
   {

ea9caaabc74e481a2c329beeb575a9bcb0935165
Shubham Shailesh Parab <shupar1996@gmail.com>
2019-08-12 12:32:36 +0530

    s120820191232

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     server: 'http://localhost:3001/OtwlGmailApp/Home.aspx',
     uploadsGA: 'http://localhost:3001/OtwlGmailApp/UploadGA.ashx',
     downloadsGA: 'http://localhost:3001/OtwlGmailApp/DownloadAttch.ashx',
-    previewGA: 'http://localhost:3001/OtwlGmailApp/PreviewAttch.ashx', 
-    uploadPdf: 'http://localhost:3001/OtwlFileServer/Upload.ashx'
+    previewGA: 'http://localhost:3001/OtwlGmailApp/PreviewAttch.ashx',
+    uploadPdf: 'http://localhost:3001/OtwlFileServer/Upload_GmailApp.ashx'
   }
 };

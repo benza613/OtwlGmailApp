54c9762e1f9362501ca1e0142538d6f34c57bb87
Pratik Mhatre <mhatrepratik44@gmail.com>
2020-03-20 17:35:40 +0530

    s20032020201735

--- csharp/postbuild.js
+++ csharp/postbuild.js
@@ -6,6 +6,7 @@ distIndexfile = 'dist/OtwlGmailApp/index.html';
 
 srcdir = 'dist/OtwlGmailApp';
 destdir = 'C:\\Users\\benza\\Source\\Repos\\IT1\\OTWLFRT\\OtwlGmailApp';
+// destdir = 'F:\\work\\IT1\\OTWLFRT\\OtwlGmailApp';
 // destdir = 'C:\\Users\\OTWL-D034\\Source\\Repos\\IT1\\OTWLFRT\\OtwlGmailApp';
 //destdir = 'D:\\work\\devp\\Ben(GMAIL)\\IT1\\OTWLFRT\\OtwlGmailApp';
 //  destdir = 'F:\\Backup of Imp IT Projects\\Changes\\2.Project with Benito Code\\20052019\\OTWLFRT\\OTWLFRT\\OtwlGmailApp';
